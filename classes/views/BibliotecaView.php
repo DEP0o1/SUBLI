efc6d9204026181ea46408fab7834035d27c8407
padrinho0-0 <lucas.alcoforado08@gmail.com>
2025-08-28 21:55:27 -0300

    views

--- classes/views/BibliotecaView.php
+++ classes/views/BibliotecaView.php
@@ -7,8 +7,23 @@ class BibliotecaView{
         $controller = new BibliotecaController;
         $bibliotecas = $controller->ListarBibliotecas($biblioteca);
 
-        
+        foreach ($bibliotecas as $Biblioteca){
 
+
+            echo" 
+        <div class='biblioteca'>
+          <img src='img/biblioteca1_1.jpg' alt='Biblioteca Mario Faria'>
+          <h4>{$Biblioteca->nm_biblioteca}</h4>
+          <p>{$Biblioteca->nm_endereco}</p>
+          <p>2.4 km de você</p>
+          <a href='Biblioteca.php'>
+            <button>Ver mais </button>
+          </a>
+          </div>";
+
+
+
+        }
 
     }
 }
