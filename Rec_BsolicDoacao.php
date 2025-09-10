7226f478b984398ed2f2c9af2240d88912f3688d
unknown <aluno@p0602.etecaf.local>
2025-09-10 11:55:40 -0300

    Reconsiderações pg livro leitor e bibliotecas

---
+++ Rec_BsolicDoacao.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title> Solicitações de doação </title>
+
+  <link rel="stylesheet" href="css/bibliotecario.css">
+  <link rel="stylesheet" href="css/mobile.css">
+  <script src="sjs/componentesJS/header.js"></script>
+    
+
+
+  <?php
+    require_once './complementos/headerBibliotecario.php';
+  ?>
+</head>
+
+<body>
+    
+</body>
---
+++ Rec_LlivroLeitor.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title> O Pequeno Príncipe </title>
+    <link rel="stylesheet" href="css/leitor.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0">
+  
+
+  </head>
+  <body>
+    <main>
+<section class="mainAreaLivro">
+
+      <img src="./img/capaLivro.jpg" alt="" class="capaLivroGrande">
+
+      <div class="pagLivro">
+
+
+        <div class="dadosLivro">
+        <h1> Coraline </h1> 
+        <p> <img src="./img/autorIcon.png" alt=""> Autor: Neil Gaiman </p>
+        <p> <img src="./img/editoraIcon.png" alt=""> Editora: HarperKids</p>
+        <p> <img src="./img/idiomaIcon.png" alt=""> Idioma: Português</p>
+        <p> <img src="./img/calendarIcon.png" alt="">Ano de publicação: 1995</p>
+      
+
+
+        <div class="botaoStatus">
+            <button class="btnRosa"> <span class="material-symbols-outlined">
+favorite
+</span> Favoritar </button>
+
+            <button class="btnRosa"> <span class="material-symbols-outlined">
+check
+</span> Reservar </button>
+            <span> <p> • Fechado </p></span>      </div>
+       
+      </div>
+
+        <div class="sinopse">  
+            <p>Um piloto cai com seu avião no deserto e ali encontra uma criança loura e frágil. Ela diz ter vindo de um pequeno 
+              planeta distante. E ali, na convivência com o piloto perdido, os dois repensam os seus valores e encontram o sentido da vid
+              a. Com essa história mágica, sensível, comovente, às vezes triste, e só aparentemente infantil, o escritor francês Antoine de Saint-Exupéry 
+              criou há 70 anos um dos maiores clássicos da ... universal. Não há adulto que não se comova ao se lembrar de quando o leu quando criança. 
+              Trata-se da maior obra existencialista do século XX, segundo Martin Heidegger. Livro mais traduzido da história, depois do Alcorão e da Bíblia, ele agora chega ao Brasil em nova edição, completa, com a tradução de Luiz Fernando Emediato e enriquecida com um caderno ilustrado sobre a obra e a curta e trágica vida do autor.</p>
+        </div>
+
+        <section class="listaBibliotecasLivro">
+        <section class="itemLista">
+          <img src="./img/bibliotecaEx.jpg" alt="" class="imgBibLista">
+        <section class="infoItemBib">
+            <div class="nmBibLista">
+          
+          <h1> Biblioteca Mario Faria <span> <p> • Fechado </p> </span></h1>
+          </div>
+        <div class="nmEndereco">
+          <img src="./img/locationPin.png.png" alt="" class="icon">
+        <p>Av. Bartolomeu Gusmão, 168 - Santos </p>
+        </div>
+        <button class="verMais">Ver mais</button>
+        </section>
+      </section>
+
+            <section class="itemLista">
+                <img src="./img/bibliotecaEx.jpg" alt="" class="imgBibLista">
+              <section class="infoItemBib">
+                  <div class="nmBibLista">
+                
+                <h1> Biblioteca Mario Faria <span> <p> • Fechado </p> </span></h1>
+                </div>
+              <div class="nmEndereco">
+                <img src="./img/locationPin.png.png" alt="" class="icon">
+              <p>Av. Bartolomeu Gusmão, 168 - Santos </p>
+              </div>
+              <button class="verMais">Ver mais</button>
+              </section>
+            </section>
+
+            <section class="itemLista">
+                <img src="./img/bibliotecaEx.jpg" alt="" class="imgBibLista">
+              <section class="infoItemBib">
+                  <div class="nmBibLista">
+                
+                <h1> Biblioteca Mario Faria <span> <p> • Fechado </p> </span></h1>
+                </div>
+              <div class="nmEndereco">
+                <img src="./img/locationPin.png.png" alt="" class="icon">
+              <p>Av. Bartolomeu Gusmão, 168 - Santos </p>
+              </div>
+              <button class="verMais">Ver mais</button>
+              </section>
+            </section>
+
+
+            <section class="itemLista">
+                <img src="./img/bibliotecaEx.jpg" alt="" class="imgBibLista">
+              <section class="infoItemBib">
+                  <div class="nmBibLista">
+                
+                <h1> Biblioteca Mario Faria <span> <p> • Fechado </p> </span></h1>
+                </div>
+              <div class="nmEndereco">
+                <img src="./img/locationPin.png.png" alt="" class="icon">
+              <p>Av. Bartolomeu Gusmão, 168 - Santos </p>
+              </div>
+              <button class="verMais">Ver mais</button>
+              </section>
+            </section>
+
+                 <section class="itemLista">
+                <img src="./img/bibliotecaEx.jpg" alt="" class="imgBibLista">
+              <section class="infoItemBib">
+                  <div class="nmBibLista">
+                
+                <h1> Biblioteca Mario Faria <span> <p> • Fechado </p> </span></h1>
+                </div>
+              <div class="nmEndereco">
+                <img src="./img/locationPin.png.png" alt="" class="icon">
+              <p>Av. Bartolomeu Gusmão, 168 - Santos </p>
+              </div>
+              <button class="verMais">Ver mais</button>
+              </section>
+            </section>
+
+                 <section class="itemLista">
+                <img src="./img/bibliotecaEx.jpg" alt="" class="imgBibLista">
+              <section class="infoItemBib">
+                  <div class="nmBibLista">
+                
+                <h1> Biblioteca Mario Faria <span> <p> • Fechado </p> </span></h1>
+                </div>
+              <div class="nmEndereco">
+                <img src="./img/locationPin.png.png" alt="" class="icon">
+              <p>Av. Bartolomeu Gusmão, 168 - Santos </p>
+              </div>
+              <button class="verMais">Ver mais</button>
+              </section>
+            </section>
+
+        </section>
+
+</section>
+</main>
+  </body>
+
+  
