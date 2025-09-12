7d342fe5b1580a1bc0db2aeeeeb154dc8c34df29
unknown <aluno@p0606.etecaf.local>
2025-09-12 09:27:54 -0300

    coiso

---
+++ re_BsolicDoacao.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Solicitações de doação</title>
+
+    <link rel="stylesheet" href="css/bibliotecario.css" />
+    <link rel="stylesheet" href="css/mobile.css" />
+    <script src="sjs/componentesJS/header.js"></script>
+  </head>
+
+  <body>
+    <?php require_once './complementos/headerBibliotecario.php';?>
+    <main>
+      <div class="tituloCentro">
+        <h1>Doações</h1>
+      </div>
+
+      <section class="areaSolicDoacao">
+        <section class="pagDoacao">
+          <div class="fotoMenordoacao">
+            <img src="img/vidas.jpg" alt="" class="imgLivroLista" />
+            <img src="img/vidas.jpg" alt="" class="imgLivroLista" />
+            <img src="img/vidas.jpg" alt="" class="imgLivroLista" />
+            <img src="img/vidas.jpg" alt="" class="imgLivroLista" />
+          </div>
+
+          <img src="img/vidas.jpg" alt="" class="capaLivroGrande" />
+        </section>
+      </section>
+    </main>
+  </body>
+</html>
