3a3e1c9fff7f8d46edc9f834216fe2f1093ed3a1
Cauã Nunes da Silva <nunescaua222@gmail.com>
2025-09-18 15:25:31 -0300

    to fazendo o umas paradas ai mas eu vou no medico e quando eu voltar eu termino

--- LlivroLeitor.php
+++ LlivroLeitor.php
@@ -1,85 +1,36 @@
 <?php
-require_once ('config.php');
+require_once('config.php');
 $codigo = null;
 if (isset($_REQUEST['codigo'])) {
-    $buscar = true;
+  $buscar = true;
 
-    if ($_REQUEST['codigo'] != "" ) {
+  if ($_REQUEST['codigo'] != "") {
 
-        $codigo = $_REQUEST['codigo'];
-        $cd_biblioteca = 1;
-    }
+    $codigo = $_REQUEST['codigo'];
+    $cd_biblioteca = 1;
+  }
 }
 
-if(isset($_REQUEST['enviado'])){
+if (isset($_REQUEST['enviado'])) {
   $controller = new ReservaController;
   $exemplarcontroller = new ExemplarController;
-  $reserva = $controller->ContarReservas(new Reserva(null,null,new Leitor(),new Livro($codigo),new Biblioteca($cd_biblioteca),true));
-  $exemplar = $exemplarcontroller->ContarExemplares(new Exemplar($codigo,$cd_biblioteca));
+  $reserva = $controller->ContarReservas(new Reserva(null, null, new Leitor(), new Livro($codigo), new Biblioteca($cd_biblioteca), true));
+  $exemplar = $exemplarcontroller->ContarExemplares(new Exemplar($codigo, $cd_biblioteca));
 
-    if($exemplar > $reserva){
-       $controller->AdicionarReserva(new Reserva(null,null,new Leitor($_SESSION['leitor']),new Livro($codigo),new Biblioteca($cd_biblioteca)));
-       $mensagem = "Livro Reservado com Sucesso";
-    }
-
-    else{
-      $mensagem = "O Livro já foi Reservado por outra Pessoa Tente novamente mais tarde";
-       // FAZER COUNT DE EXEMPLARES
-    }
-   
+  if ($exemplar > $reserva) {
+    $controller->AdicionarReserva(new Reserva(null, null, new Leitor($_SESSION['leitor']), new Livro($codigo), new Biblioteca($cd_biblioteca)));
+    $mensagem = "<h1>Livro Reservado com Sucesso</h1>";
+  } else {
+    $mensagem = "<h1>O Livro já foi Reservado por outra Pessoa Tente novamente mais tarde</h1>";
+    // FAZER COUNT DE EXEMPLARES
+  }
 }
 
 ?>
 
 
 <!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title> O Pequeno Príncipe </title>
-    <link rel="stylesheet" href="css/bibliotecario.css" />
-    <link rel="stylesheet" href="css/livroBibliotecario.css">
-    <link rel="stylesheet" href="css/leitor.css">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0">
-    <script src="js/componentesJS/reserva.js" declare></script>
-
-
-    <?php require_once './complementos/headerLeitor.php'; ?>  
-
-  </head>
-  <body>
-
-    <main class="areaLivroEmprestado">
-  <section class="livroEmprestado">
-      <?php
-      if($buscar)
-      $livro = new LivroView;
-      $livro->ExibirLivro(new Livro($codigo));
-      ?>
-  </section>
-  </main>
-
-  <section class="areaBtn">
-    <form class="btnEmprestimo" method="GET" action="">
-      <input type="hidden" name="codigo" value="<?=$codigo?>">
-      <input type="hidden" name="enviado" value="true">
-      <button class="btnRosa" id="s" type="submit">Reservar</button>
-      <?php
-    if(isset($_REQUEST['enviado'])){
-        echo $mensagem;
-      }
-      ?>
-    </form>
-  </section>
-
-  </body>
-</html>
-
-
-
-<!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 
 <head>
   <meta charset="UTF-8" />
@@ -87,108 +38,34 @@ if(isset($_REQUEST['enviado'])){
   <title> O Pequeno Príncipe </title>
   <link rel="stylesheet" href="css/leitor.css">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@48,400,0,0">
+  <script src="js/componentesJS/reserva.js" declare></script>
+
+
+  <?php require_once './complementos/headerLeitor.php'; ?>
+
 </head>
 
 <body>
-  <?php include 'complementos/headerLeitor.php'; ?>
+
   <main>
-    <section class="mainAreaLivro">
+    <section class="AreaLivro">
 
-      <img src="img/vidas.jpg" alt="" class="capaLivroGrande">
+      <?php
+      if ($buscar)
+        $livro = new LivroView;
+      $livro->ExibirLivro(new Livro($codigo));
+      ?>
 
-      <div class="pagLivro">
+      <!-- <?= $codigo ?> -->
 
-        <div class="dadosLivro">
-          <h1> Coraline </h1>
+      <?php
+      if (isset($_REQUEST['enviado'])) {
+        echo $mensagem;
+      }
+      ?>
 
-          <p>
-            <span class="material-symbols-outlined">
-              man_4
-            </span> Autor: Neil Gaiman
-          </p>
-
-          <p>
-            <span class="material-symbols-outlined">
-              corporate_fare
-            </span> Editora: HarperKids
-          </p>
-
-          <p>
-            <span class="material-symbols-outlined">
-              language
-            </span> Idioma: Português
-          </p>
-
-          <p>
-            <span class="material-symbols-outlined">
-              calendar_month
-            </span> Ano de publicação: 1995
-          </p>
-
-          <div class="botaoStatus">
-            <button class="btnRosa"> <span class="material-symbols-outlined">
-                favorite
-              </span> Favoritar </button>
-
-            <button class="btnRosa"> <span class="material-symbols-outlined">
-                check
-              </span> Reservar </button>
-            <span>
-              <p> • Fechado </p>
-            </span>
-          </div>
-
-        </div>
-
-        <div class="sinopse">
-          <p>Um piloto cai com seu avião no deserto e ali encontra uma criança loura e frágil. Ela diz ter vindo de um pequeno
-            planeta distante. E 
-            Trata-se da maior obra existencialista do século XX, segundo Martin Heidegger. Livro mais traduzido da história, depois do Alcorão e da Bíblia, ele agora chega ao Brasil em nova edição, completa, com a tradução de Luiz Fernando Emediato e enriquecida com um caderno ilustrado sobre a obra e a curta e trágica vida do autor.</p>
-        </div>
-
-        <section class="listaBibliotecasLivro">
-
-          <div class="itemLista">
-            <img src="img/biblioteca1_1.jpg" alt="" class="imgBibLista">
-            <div class="infoItemBib">
-              <div class="nmBibLista">
-                <h1> Biblioteca Mario Faria <span>
-                    <p> • Fechado </p>
-                  </span></h1>
-              </div>
-              <div class="nmEndereco">
-                <img src="./img/locationPin.png.png" alt="" class="icon">
-
-                <p>
-                <span class="material-symbols-outlined">
-                location_on
-                </span> 
-                Av. Bartolomeu Gusmão, 168 - Santos  
-              </p>
-
-              </div>
-              <button class="verMais">Ver mais</button>
-            </div>
-          </div>
-
-          <div class="itemLista">
-            <img src="img/biblioteca1_1.jpg" alt="" class="imgBibLista">
-            <div class="infoItemBib">
-              <div class="nmBibLista">
-                <h1> Biblioteca Mario Faria <span>
-                    <p> • Fechado </p>
-                  </span></h1>
-              </div>
-              <div class="nmEndereco">
-                <img src="./img/locationPin.png.png" alt="" class="icon">
-                <p>Av. Bartolomeu Gusmão, 168 - Santos </p>
-              </div>
-              <button class="verMais">Ver mais</button>
-            </div>
-          </div>
-        </section>
 
     </section>
   </main>
 </body>
-
+</html>
--- classes/views/LivroView.php
+++ classes/views/LivroView.php
@@ -13,6 +13,9 @@ class LivroView{
         <div class='livro'>
             <img src='img/{$Livro->cd_livro}'/></img>
             <h2>{$Livro->nm_livro}</h2>
+            <div class='favorito'>
+              <span class='material-symbols-outlined'>favorite</span>
+            </div>
         ";
 
         foreach($Livro->autores as $autor){
@@ -23,75 +26,19 @@ class LivroView{
         }
         echo
         "
-            <button><a href='LlivroLeitor.php?codigo=$Livro->cd_livro'>Ver Mais</a></button>
+            <button class='btnRosa'><a href='LlivroLeitor.php?codigo=$Livro->cd_livro'>Ver Mais</a></button>
         </div>
         ";
     }
-
-
 }
 
-    public function ExibirLivro($livro = new Livro()){
-
-       
-        $controller = new LivroController;
-        $livros = $controller->ListarLivros($livro);
-
-        $Bcontroller = new BibliotecaController;
-        $bibliotecas = $Bcontroller->ListarBibliotecas(new Biblioteca (null,null,null,[new Livro($livros[0]->cd_livro)]));
-    
-    foreach($livros as $Livro){
-        echo 
-        "
-        <img src='img/{$Livro->cd_livro}' class='capaLivroEmprestado'></img>
-    <div class='infoLivro'>
-    <h1> $Livro->nm_livro </h1>";
-
-
-    foreach($Livro->autores as $autor){
-      echo"
-      <h2> Autor:$autor->nm_autor  </h2>
-      ";
-    }
-    $editoracontroller = new EditoraController();
-    $editora = $editoracontroller->ListarEditoras(new Editora(null,null,$Livro->cd_livro));
-      echo"
-      <h2> Editora:{$editora[0]->nm_editora} </h2>
-      ";
-   echo"
-    <h2> Disponivel em: ";
-
-    foreach($bibliotecas as $Biblioteca){
-      echo"
-      $Biblioteca->nm_biblioteca </h2>";
-}
-    echo"
-      <h2> Status: Disponível  </h2>
-
-      <div class='linha'> </div>
-
-      <h1> Sinopse </h1>
-      <p>$Livro->ds_sinopse</p>
-    </div>"
-
-
-        ;
-
-
-
-  }
-     }
-    }
-
-?>
-
-<!-- public function ExibirLivro($livro = new Livro()){
+   public function ExibirLivro($livro = new Livro()){
 
     $controller = new LivroController;
     $livros = $controller->ListarLivros($livro);
 
     $Bcontroller = new BibliotecaController;
-    $bibliotecas = $Bcontroller->ListarBibliotecas(new Biblioteca (null,null,null,[new Livro($livros[0]->cd_livro)]));
+    $bibliotecas = $Bcontroller->ListarBibliotecas(new Biblioteca(null,null,null,[new Livro($livros[0]->cd_livro)]));
 
     foreach($livros as $Livro){
 
@@ -99,12 +46,14 @@ class LivroView{
         $editora = $editoracontroller->ListarEditoras(new Editora(null,null,$Livro->cd_livro));
 
         echo "
-        <img src='img/{$Livro->cd_livro}.jpg' alt='' class='capaLivroGrande'>
+        <div class='capaLivroGrande'>
+            <img src='img/{$Livro->cd_livro}' alt=''>
+        </div>
 
         <div class='pagLivro'>
 
           <div class='dadosLivro'>
-            <h1> {$Livro->nm_livro} </h1>";
+            <h1>{$Livro->nm_livro}</h1>";
 
         foreach($Livro->autores as $autor){
             echo "
@@ -122,41 +71,52 @@ class LivroView{
 
             <p>
               <span class='material-symbols-outlined'>language</span>
-              Idioma: {$Livro->ds_idioma}
+              Idioma: aaa
             </p>
 
             <p>
               <span class='material-symbols-outlined'>calendar_month</span>
-              Ano de publicação: {$Livro->ano_publicacao}
+              Ano de publicação: aaa
             </p>
 
-            <div class='botaoStatus'>
+            <section class='areaBtn'>
               <button class='btnRosa'>
                 <span class='material-symbols-outlined'>favorite</span>
                 Favoritar
               </button>
 
-              <button class='btnRosa'>
-                <span class='material-symbols-outlined'>check</span>
-                Reservar
-              </button>
-              <span><p>• Disponível em: ";
-
-        foreach($bibliotecas as $Biblioteca){
-            echo " {$Biblioteca->nm_biblioteca} ";
+              <form class='btnEmprestimo' method='GET' action=''>
+                <input type='hidden' name='codigo' value='{$Livro->cd_livro}'>
+                <input type='hidden' name='enviado' value='true'>
+                <button class='btnRosa' id='s' type='submit'>
+                  <span class='material-symbols-outlined'>check</span>
+                  Reservar
+                </button>";
+        
+        if (isset($_REQUEST['enviado'])) {
+            echo '';
         }
 
-        echo "</p></span>
-            </div>
+        echo "
+              </form>
+            </section>
+            <span>
+              <p>• Disponível em: ";
+        
+        foreach($bibliotecas as $Biblioteca){
+            echo "{$Biblioteca->nm_biblioteca} ";
+        }
+
+        echo "</p>
+            </span>
           </div>
 
           <div class='sinopse'>
             <p>{$Livro->ds_sinopse}</p>
           </div>
-
         </div>
         ";
     }
-} -->
-
-
+}
+    }
+?>
