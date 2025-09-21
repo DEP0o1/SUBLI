28fc5fcdca5230783d9dcaac95868d07b51085ae
Cauã Nunes da Silva <nunescaua222@gmail.com>
2025-09-21 16:25:09 -0300

    menu bibliotecario em andamento bem gostosinho

--- js/componentesJS/menuBibliotecario.js
+++ js/componentesJS/menuBibliotecario.js
@@ -14,41 +14,85 @@ document.addEventListener("DOMContentLoaded", () => {
 <br>
 <div class="conteudo-aside">
   <div class="conteudo-aside-biblioteca">
-    <h1>Biblioteca</h1>
 
+  <h1>Biblioteca</h1>
+
+    <div class="informacao-aside" >
+      <span class="material-symbols-outlined">library_add</span>
+      <a href="BcadastrarLivro.php">
+        <p>Perfil da biblioteca</p>
+      </a>
+    </div>
+
+    <div class="informacao-aside" >
+      <span class="material-symbols-outlined">library_add</span>
+      <a href="BcadastrarLivro.php">
+        <p>Acervo</p>
+      </a>
+    </div>
+
+    </div>
+
+    <div class="conteudo-aside-biblioteca">
+    <h1>Leitores</h1>
     <div class="informacao-aside">
+      <span class="material-symbols-outlined">groups</span>
+      <p>Todos os leitores</p>
+    </div>
+
+     <div class="informacao-aside">
+      <span class="material-symbols-outlined">groups</span>
+      <p>Todos os leitores</p>
+    </div>
+
+     <div class="informacao-aside">
+      <span class="material-symbols-outlined">groups</span>
+      <p>Todos os leitores</p>
+    </div>
+
+     <div class="informacao-aside">
+      <span class="material-symbols-outlined">groups</span>
+      <p>Todos os leitores</p>
+    </div>
+  </div>
+
+    <div class="conteudo-aside-leitores">
+
+    <h1>Cadastros</h1>
+
+    <div class="informacao-aside" >
       <span class="material-symbols-outlined">library_add</span>
       <a href="BcadastrarLivro.php">
         <p>Cadastrar Livro</p>
       </a>
     </div>
 
-    <div class="informacao-aside">
+    <div class="informacao-aside" >
       <span class="material-symbols-outlined">person_add</span>
       <p>Cadastrar Autor</p>
     </div>
 
-    <div class="informacao-aside">
+    <div class="informacao-aside" >
       <span class="material-symbols-outlined">category</span>
       <p>Cadastrar Gênero</p>
     </div>
     
-    <div class="informacao-aside">
+    <div class="informacao-aside" >
       <span class="material-symbols-outlined">translate</span>
       <p>Cadastrar Idioma</p>
     </div>
 
-    <div class="informacao-aside">
+    <div class="informacao-aside" id="abrir-editora">
       <span class="material-symbols-outlined">business</span>
       <p>Cadastrar Editora</p>
     </div>
 
-    <div class="informacao-aside">
+    <div class="informacao-aside" >
       <span class="material-symbols-outlined">collections_bookmark</span>
       <p>Cadastrar Coleções</p>
     </div>
 
-    <div class="informacao-aside">
+    <div class="informacao-aside" >
       <span class="material-symbols-outlined">event</span>
       <p>Cadastrar Evento</p>
     </div>
@@ -57,14 +101,6 @@ document.addEventListener("DOMContentLoaded", () => {
           <span class="material-symbols-outlined">topic</span>
           <p>Cadastrar Assunto</p>
         </div>
-
-  </div>
-  <div class="conteudo-aside-leitores">
-    <h1>Leitores</h1>
-    <div class="informacao-aside">
-      <span class="material-symbols-outlined">groups</span>
-      <p>Todos os leitores</p>
-    </div>
   </div>
 </div>
   `;
@@ -84,10 +120,72 @@ document.addEventListener("DOMContentLoaded", () => {
   aside.querySelector(".btn-fechar").addEventListener("click", fechar);
   overlay.addEventListener("click", fechar);
 
-/*-----------------------------assunto------------------------------------------------------------------------*/
+  /*-----------------------------livro------------------------------------------------------------------------*/
+
+  /*-----------------------------autor------------------------------------------------------------------------*/
+
+  /*-----------------------------genero------------------------------------------------------------------------*/
+
+  /*-----------------------------idioma------------------------------------------------------------------------*/
+
+  /*-----------------------------editora------------------------------------------------------------------------*/
+
+  const botaoAbrirEditora = aside.querySelector("#abrir-editora");
+  botaoAbrirEditora.addEventListener("click", () => {
+    const overlayPopup = document.createElement("div");
+    overlayPopup.classList.add("overlayPopup");
+    document.body.appendChild(overlayPopup);
+
+    const popup = document.createElement("div");
+    popup.className = "areaCadastro";
+    popup.innerHTML = `
+      <form class="formAvancado1">
+        <div class="titulo-area-cadastro">
+          <h1>Cadastrar Editora</h1>
+          <button type="button" id="fechar-popup">
+            <span class="material-symbols-outlined">close</span>
+          </button>
+        </div>
+        <section class="areaInput">
+          <div class="areaTituloLivro">
+            <label>Código da editora:</label>
+            <input type="text" placeholder="Ex: 1">
+          </div>
+          <div class="areaTituloLivro">
+            <label>Nome da editora</label>
+            <input type="text" placeholder="Ex: Jambo Editora">
+          </div>
+          <div class="areaBtn">
+            <button class="btnRosa">Cadastrar</button>
+          </div>
+        </section>
+      </form>
+    `;
+    document.body.appendChild(popup);
+
+    popup.querySelector("#fechar-popup").addEventListener("click", () => {
+      document.body.removeChild(popup);
+      document.body.removeChild(overlayPopup);
+      overlayPopup.addEventListener("click", fechar);
+    });
+    overlayPopup.addEventListener("click", () => {
+      document.body.removeChild(popup);
+      document.body.removeChild(overlayPopup);
+    });
+  });
+
+  /*-----------------------------coleçoes------------------------------------------------------------------------*/
+
+  /*-----------------------------evento------------------------------------------------------------------------*/
+
+  /*-----------------------------assunto------------------------------------------------------------------------*/
 
   const botaoAbrirAssunto = aside.querySelector("#abrir-assunto");
   botaoAbrirAssunto.addEventListener("click", () => {
+    const overlayPopup = document.createElement("div");
+    overlayPopup.classList.add("overlayPopup");
+    document.body.appendChild(overlayPopup);
+
     const popup = document.createElement("div");
     popup.className = "areaCadastro";
     popup.innerHTML = `
@@ -117,6 +215,12 @@ document.addEventListener("DOMContentLoaded", () => {
 
     popup.querySelector("#fechar-popup").addEventListener("click", () => {
       document.body.removeChild(popup);
+      document.body.removeChild(overlayPopup);
+    });
+
+    overlayPopup.addEventListener("click", () => {
+      document.body.removeChild(popup);
+      document.body.removeChild(overlayPopup);
     });
   });
 });
