1b2c41e5efddcf92b44748f40828d582765b8fc0
unknown <alicya.santos.n@gmail.com>
2025-09-30 21:07:55 -0300

    refiz a solicitacao de evento

--- BsolicEvento.php
+++ BsolicEvento.php
@@ -8,10 +8,10 @@
   <link rel="stylesheet" href="css/bibliotecario.css">
   <link rel="stylesheet" href="css/mobile.css">
   <script src="js/componentesJS/header.js"></script>
-    
+
 
   <?php
-    require_once './complementos/headerBibliotecario.php';
+  require_once './complementos/headerBibliotecario.php';
   ?>
 </head>
 
@@ -19,24 +19,40 @@
 
   <section class="evento">
 
-    <div class="titulos">
+    <img src="img/doar.png" alt="" class="imgEvento">
+
+    <div class="eventoTxt">
       <h1> Divulgação do livro "A vida de Terry Crews" </h1>
 
       <div class="info">
-        <h2> Data: 10/03/2025 - 20/03/2025</h2>
-        <h2> Horário: 14:20 - 18:40 </h2>
+        <p>
+          <span class="material-symbols-outlined">
+            calendar_month
+          </span> Data: 10/03/2025 - 20/03/2025
+        </p>
+        <p>
+          <span class="material-symbols-outlined">
+            nest_clock_farsight_analog
+          </span> Horário: 14:20 - 18:40
+        </p>
       </div>
-    </div>
 
-    <p> Eu gostaria de fazer a divulgação do meu livro nesta biblioteca, esse livro é minha autobiografia que conta a história de como eu me tornei um ator famoso e renomado de um jeito leve e descontraído, quando eu pesquisei um pouco eu descobri que sou muito famoso no Brasil, e então comecei uma viagem afim de divulgar meu livro e estou indo em bibliotecas do Brasil inteiro para fazer essa divulgação, bom nesse período de tempo que eu estou solicitando eu farei alguns sorteios dos meus livros, farei uma explicação do porque o escrevi, também responderei perguntas e tirarei fotos, espero que vocês aceitem essa solicitação e que possamos fazer um evento incrível. </p>
+      <p> Eu gostaria de fazer a divulgação do meu livro nesta biblioteca, esse livro é minha autobiografia que conta a história de como eu me tornei um ator famoso e renomado de um jeito leve e descontraído, quando eu pesquisei um pouco eu descobri que sou muito famoso no Brasil, e então comecei uma viagem afim de divulgar meu livro e estou indo em bibliotecas do Brasil inteiro para fazer essa divulgação, bom nesse período de tempo que eu estou solicitando eu farei alguns sorteios dos meus livros, farei uma explicação do porque o escrevi, também responderei perguntas e tirarei fotos, espero que vocês aceitem essa solicitação e que possamos fazer um evento incrível. </p>
 
-    <div class="botoes">
-      <button class="aceitar"> Aceitar </button>
-      <button class="recusar"> Recusar </button>
+      <div class="areaBtnEvento">
+        <button class="btnRosa"> Aceitar </button>
+        <button class="btnRosa"> Recusar </button>
+        <div class="usuarioEvento">
+              <img src="https://cdn.sfstation.com/assets/images/events/08/24802081856853977_orig.jpg" alt="">
+              <p>Adamastor</p>
+              <h3>(Responsável)</h3>
+            </div>
+      </div>
     </div>
 
   </section>
 
 
 </body>
+
 </html>
